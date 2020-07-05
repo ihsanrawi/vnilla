a6901de0cf7941bf2a6e6f126ab5b31e58514270
Ihsan <ihsanrawi@gmail.com>
2020-07-05 16:31:41 +0800

    added react native navvigation and gesture handler

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
  * @format
  */
 
+import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import TrackPlayer from 'react-native-track-player';
 import App from './src/App';
 import { name as appName } from './app.json';
-import PlayerService from './src/utils/TrackPlayerService';
+import PlayerService from './src/services/TrackPlayerService';
 
 AppRegistry.registerComponent(appName, () => App);
 TrackPlayer.registerPlaybackService(() => PlayerService.trackControlListener());
