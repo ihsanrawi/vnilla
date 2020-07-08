38fc479c0fafc9aa3edfd0a37e81ae6562367f87
Ihsan <ihsanrawi@gmail.com>
2020-07-08 23:07:53 +0800

    Added svg package and its configuration

--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,24 @@
  * @format
  */
 
-module.exports = {
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: assetExts.filter((ext) => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
+
+/* module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -14,4 +31,4 @@ module.exports = {
       },
     }),
   },
-};
+}; */
