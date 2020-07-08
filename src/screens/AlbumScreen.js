335bf89b44ad4f6127849680aadb078b7106d1f8
Ihsan <ihsanrawi@gmail.com>
2020-07-08 21:40:25 +0800

    Initial commit

---
+++ src/screens/AlbumScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const AlbumScreen = () => {
+  return (
+    <View>
+      <Text>AlbumScreen</Text>
+    </View>
+  );
+};
+
+export default AlbumScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/ArtistScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const ArtistScreen = () => {
+  return (
+    <View>
+      <Text>ArtistScreen</Text>
+    </View>
+  );
+};
+
+export default ArtistScreen;
+
+const styles = StyleSheet.create({});
