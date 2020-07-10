51569c7b7cd286a09f744dd16d9586e4f454d352
Ihsan <ihsanrawi@gmail.com>
2020-07-10 20:56:04 +0800

    Uses native View component

--- index.js
+++ index.js
@@ -5,10 +5,12 @@
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import TrackPlayer from 'react-native-track-player';
+import { enableScreens } from 'react-native-screens';
 import App from './src/App';
 import { name as appName } from './app.json';
-import PlayerService from './src/services/TrackPlayerService';
 import bgService from './src/services/RemoteControlListener';
+
+enableScreens();
 
 AppRegistry.registerComponent(appName, () => App);
 TrackPlayer.registerPlaybackService(() => bgService);
